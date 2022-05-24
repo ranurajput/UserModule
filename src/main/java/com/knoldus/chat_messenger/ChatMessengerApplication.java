0242fceba07a381bb5887fc4b20126532ee9e72d
ranurajput <rrajput9760@gmail.com>
2022-05-24 20:44:15 +0530

    Initial commit

---
+++ src/main/java/com/knoldus/chat_messenger/ChatMessengerApplication.java
@@ -0,0 +1,14 @@
+package com.knoldus.chat_messenger;
+
+import com.knoldus.chat_messenger.validation.UserValidation;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ChatMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatMessengerApplication.class, args);
+
+	}
+
+}
